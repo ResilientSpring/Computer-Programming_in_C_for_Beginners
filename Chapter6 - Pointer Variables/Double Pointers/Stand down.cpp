25b4fd95a9d1c7f4af2ce3a4464f3067a904c37b
Ceiling_roof <ted10014@hotmail.com>
2022-06-07 13:04:44 +0800

    Committed 2022/06/07

---
+++ Chapter6 - Pointer Variables/Double Pointers/Stand down.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <cctype>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int **double_pointer = NULL, * i_ptr = NULL;
+
+}
