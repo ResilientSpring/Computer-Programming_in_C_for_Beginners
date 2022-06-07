88f266b5d4fca00b6d531c05f69176c24823b730
Ceiling_roof <ted10014@hotmail.com>
2022-06-07 13:23:16 +0800

    Committed 2022/06/07

--- Chapter6 - Pointer Variables/Double Pointers/Stand down.cpp
+++ Chapter6 - Pointer Variables/Double Pointers/Stand down.cpp
@@ -7,6 +7,18 @@
 using namespace std;
 
 int main() {
+	// In a declaration statement, preceding a variable with * causes that variable to be a pointer.
 	int **double_pointer = NULL, * i_ptr = NULL;
+
+		// A pointer is an object/variable that can contain a memory address.
+
+	// ------- End of declaration statement.
+
+	int ultimate_variable = 100;
+
+	// i_ptr is an object/variable that can contain a memory address of an integer.
+	i_ptr = &ultimate_variable;   // & is an operator standing for the memory address of its operand.
+	double_pointer = &i_ptr;
+
 
 }
